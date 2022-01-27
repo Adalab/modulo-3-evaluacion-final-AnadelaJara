bd5784cfe56e9d004058c96176b2355253e15049
AnadelaJara <anadelajara@gmail.com>
2022-01-27 09:50:22 +0100

    add some icons

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -4,27 +4,27 @@ const CharacterDetail = (props) => {
 
   const getSpecies = () => {
     if (props.character.species === "human") {
-      return "Humano";
+      return <p>Humano <i className="far fa-user"></i></p>;
     } else if (props.character.species === "half-giant") {
-      return "Medio-gigante";
+      return <p>Medio-gigante</p>;
     } else if (props.character.species === "werewolf") {
-      return "Hombre-lobo";
+      return <p>Hombre-lobo</p>;
     } else if (props.character.species === "goblin") {
-      return "Duende";
+      return <p>Duende</p>;
     } else if (props.character.species === "ghost") {
-      return "Fantasma";
+      return <p>Fantasma <i className="fas fa-ghost"></i></p>;
     } else if (props.character.species === "house-elf") {
-      return "Elfo doméstico";
+      return <p>Elfo doméstico</p>;
     } else if (props.character.species === "poltergeist") {
-      return "Poltergeist";
+      return <p>Poltergeist</p>;
     } else if (props.character.species === "giant") {
-      return "Gigante";
+      return <p>Gigante</p>;
     } else if (props.character.species === "centaur") {
-      return "Centauro";
+      return <p>Centauro</p>;
     } else if (props.character.species === "half-human") {
-      return "Medio-humano";
+      return <p>Medio-humano</p>;
     } else if (props.character.species === "owl") {
-      return "Búho";
+      return <p>Búho</p>;
     }
   };
 
@@ -33,15 +33,14 @@ const CharacterDetail = (props) => {
   };
 
   const getStatus = () => {
-    // return props.character.status ? "Vivo" : " Muerto";
     if (props.character.status && props.character.gender === 'female') {
-      return 'Viva';
+      return <p>Viva  < i className="fas fa-heartbeat" ></i ></p>
     } else if (props.character.status && props.character.gender === 'male') {
-      return 'Vivo';
+      return <p>Vivo  < i className="fas fa-heartbeat" ></i ></p>
     } else if (!props.character.status && props.character.gender === 'female') {
-      return 'Muerta';
+      return <p>Muerta <i className="fas fa-skull"></i></p>
     } else if (!props.character.status && props.character.gender === 'male') {
-      return 'Muerto';
+      return <p>Muerto <i className="fas fa-skull"></i></p>
     }
   };
 
